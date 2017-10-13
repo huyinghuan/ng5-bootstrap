974cebe34353cad1a4c800f9e01707f79fa5f33f
hyh <ec.huyinghuan@gmail.com>
2017-10-13 15:09:08 +0800

    add unsubscription

--- src/app/components/second.ts
+++ src/app/components/second.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
+import { Observable} from 'rxjs/Observable';
+import {ISubscription} from 'rxjs/Subscription'
 let template:string = 
 `
 <h2>Second</h2>
@@ -14,15 +15,32 @@ let template:string =
   selector: 'second-route',
   template: template
 })
-export class SecondComponent implements OnInit  {
+export class SecondComponent implements OnInit, OnDestroy {
+  private subscriptQueryParams:ISubscription
+  private subscriptParams:ISubscription
   params = {}
   constructor( private route: ActivatedRoute){}
   test(){
-    console.log(this.params["id"])
+    console.log(this.params)
   }
   ngOnInit() {
-    this.route.params.subscribe((params)=>{ 
+    /** 
+     * 非箭头函数 this 指向 function内部,无法使用this.params指向外部
+    this.route.params.subscribe(function(params){ 
       this.params = params
     })
+    */
+    this.subscriptQueryParams = this.route.queryParams.subscribe((params)=>{
+      Object.assign(this.params, params)
+    })
+    this.subscriptParams = this.route.params.subscribe((params)=>{
+      Object.assign(this.params, params)
+    })
+  }
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscriptQueryParams.unsubscribe()
+    this.subscriptParams.unsubscribe()
   }
 }
